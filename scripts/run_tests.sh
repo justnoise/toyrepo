e4056a930038d24f41c7dddb4b805fdd46630a36
Brendan Cox <bcox@elotl.co>
2018-11-29 14:17:53 -0800

    testing out suggestion for finding local branch name

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -9,12 +9,13 @@ cd $ROOT_DIR
 echo "root directory: $ROOT_DIR"
 
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
+GIT_BRANCH_LOCAL=$(echo $GIT_BRANCH   | sed -e "s|origin/||g")
 CURRENT_TAG=$(git tag -l --points-at HEAD | head -n 1)
 
 sleep 10
 
 echo "current branch: $CURRENT_BRANCH"
+echo "git local branch: $GIT_BRANCH_LOCAL"
 echo "current tag: $CURRENT_TAG"
 
 if [[ $CURRENT_TAG =~ ^v[0-9].* ]]; then
