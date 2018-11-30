3cae4721811f3c7a3370e04a9b2000aae8fb89ed
Brendan Cox <bcox@elotl.co>
2018-11-29 17:57:10 -0800

    update to grab current tag and branch

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -8,8 +8,7 @@ cd $ROOT_DIR
 
 echo "root directory: $ROOT_DIR"
 
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-GIT_BRANCH_LOCAL=$(echo $GIT_BRANCH   | sed -e "s|origin/||g")
+CURRENT_BRANCH=$(echo $GIT_BRANCH   | sed -e "s|origin/||g")
 CURRENT_TAG=$(git tag -l --points-at HEAD | head -n 1)
 
 sleep 10
@@ -18,12 +17,14 @@ echo "current branch: $CURRENT_BRANCH"
 echo "git local branch: $GIT_BRANCH_LOCAL"
 echo "current tag: $CURRENT_TAG"
 
-if [[ $CURRENT_TAG =~ ^v[0-9].* ]]; then
-    echo "we are currently on a release tag"
+if [[ $CURRENT_BRANCH == "master" ]]; then
+    echo "detected that we are currently on master branch"
+else
+    echo "detected that we are currently on $CURRENT_BRANCH branch"
 fi
 
-if [[ $CURRENT_BRANCH == "master" ]]; then
-    echo "we are currently on master"
+if [[ $CURRENT_TAG =~ ^v[0-9].* ]]; then
+    echo "we are currently on a release tag"
 fi
 
 echo "done"
